c68ccce32e30d93552d2be0ea0ad787b9a22215f
babatunde tolulope <babsbright69@gmail.com>
2024-07-19 23:04:18 +0100

    uploaded frontend

--- frontend/app/utils/auth.tsx
+++ frontend/app/utils/auth.tsx
@@ -1,25 +1,26 @@
 "use client";
-import {useEffect } from "react";
+import { useEffect } from "react";
 import Cookies from "universal-cookie";
 import { useRouter } from "next/navigation";
 
 
 export default function IsAuth(Component: any) {
-
-  return (props:any) => {
+  return function isAuth(props: any) {
     const cookies = new Cookies();
     const router = useRouter()
     const token = cookies.get("TOKEN");
+
     useEffect(() => {
-      // If the user is not authenticated, redirect to the login page
       if (!token) {
-        router.push('/login');
+        router.push("/");
       }
-         // eslint-disable-next-line react-hooks/exhaustive-deps
+
     }, []);
 
-    // If the user is authenticated, render the WrappedComponent
-    // Otherwise, render null while the redirection is in progress
-    return token ? <Component {...props} /> : null;
-  };
+    if (!token) {
+      router.push("/");
+      return null;
+    }
+    return <Component {...props} />;
+  }
 }
