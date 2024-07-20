6ddfe84a82afc81ad7b2a23e8c3e899af1816e61
babatunde tolulope <babsbright69@gmail.com>
2024-07-20 10:57:07 +0100

    frontend added

--- frontend/app/components/Charts/page.tsx
+++ frontend/app/components/Charts/page.tsx
@@ -2,12 +2,7 @@
 import Breadcrumb from "../Breadcrumbs/Breadcrumb";
 import ChartOne from "../Charts/ChartOne";
 import ChartTwo from "../Charts/ChartTwo";
-import dynamic from "next/dynamic";
 import React from "react";
-
-const ChartThree = dynamic(() => import("../Charts/ChartThree"), {
-  ssr: false,
-});
 
 const Chart: React.FC = () => {
   return (
@@ -17,7 +12,6 @@ const Chart: React.FC = () => {
       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
       </div>
     </>
   );
